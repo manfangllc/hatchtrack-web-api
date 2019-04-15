d9bb8b388035a9cb06b07f62aab0b4649b169e35
Michael Reutman <mreutman@gmail.com>
2019-04-15 16:33:30 -0700

    clean up and working on auth

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
-const pool = new Pool({
+// configuration //////////////////////////////////////////////////////////////
+
+const postgresPool = new Pool({
   user: 'master',
   host: 'hatchtrack-peep-user-db-test.crprnnsms8xr.us-west-2.rds.amazonaws.com',
   database: 'hatchtrack_peep_user_db_test',
@@ -12,18 +14,48 @@ const pool = new Pool({
   port: 5432,
 });
 
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'development') {
+  app.set('json spaces', 2);
+}
+
+app.use((err, req, res, next) => {
+  if (err !== null) {
+    return res.status(400).send();
+  }
+  return next();
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
+);
+
+// routes /////////////////////////////////////////////////////////////////////
+
+app.post("/v1/auth", (req, res) => {
+  try {
+    console.log(req.body);
+    auth.authEmail("test@widgt.ninja", "Test1337$");
+    res.status(200).send();
+  }
+  catch (err) {
+    res.status(422).send();
+  }
+});
+
 app.get('/v1/email2uuids', (req, res) => {
 
   var email = req.query.email;
   if (!email) {
-    res.status(422).json({ message : "missing query parameter"});
+    res.status(422).send();
   }
   else {
     var q = "";
     q += "SELECT peep_uuids FROM email_2_peep_uuids ";
     q += "WHERE email='" + email + "'";
 
-    pool.query(q, (err, result) => {
+    postgresPool.query(q, (err, result) => {
       if (err) {
         throw err;
       }
@@ -37,14 +69,14 @@ app.get('/v1/uuid2info', (req, res) => {
 
   var uuid = req.query.uuid;
   if (!uuid) {
-    res.status(422).json({ message : "missing query parameter"});
+    res.status(422).send();
   }
   else {
     var q = "";
     q += "SELECT name FROM peep_uuid_2_info ";
     q += "WHERE uuid='" + uuid + "'";
 
-    pool.query(q, (err, result) => {
+    postgresPool.query(q, (err, result) => {
       if (err) {
         throw err;
       }
@@ -53,11 +85,3 @@ app.get('/v1/uuid2info', (req, res) => {
     });
   }
 });
-
-if(process.env.NODE_ENV === 'development') {
-  app.set('json spaces', 2);
-}
-
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
-);
